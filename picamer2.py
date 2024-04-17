c20cf0b39ed6244625f08bb977e7e3fec9542014
Thomas Brinck-Mortensen <thomasbr@uia.no>
2024-04-17 03:31:05 +0200

    working_videostream

--- picamer2.py
+++ picamer2.py
@@ -6,9 +6,7 @@ from picamera2 import Picamera2
 cv2.startWindowThread()
 
 picam2 = Picamera2()
-preview_config = picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
-preview_config["main"]["roi"] = (0.0, 0.0, 1.0, 1.0)
-picam2.configure(preview_config)
+picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (1080, 1080)}))
 picam2.start()
 
 while True:
