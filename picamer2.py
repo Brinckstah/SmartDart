0dd0720109bc9d10e0d359d7e916649766627a36
Thomas Brinck-Mortensen <thomasbr@uia.no>
2024-04-17 03:09:54 +0200

    test_zoom

--- picamer2.py
+++ picamer2.py
@@ -6,7 +6,9 @@ from picamera2 import Picamera2
 cv2.startWindowThread()
 
 picam2 = Picamera2()
-picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
+preview_config = picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
+preview_config["main"]["roi"] = (0.0, 0.0, 1.0, 1.0)
+picam2.configure(preview_config)
 picam2.start()
 
 while True:
