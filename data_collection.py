11faa0114e608dd64293bafeb2fdb91c1f19598e
Filip Guttormsen <filipg@uia.no>
2024-04-30 18:15:42 +0200

    feat: created servo_controller class

--- data_collection.py
+++ data_collection.py
@@ -2,6 +2,51 @@ import cv2
 import os
 import re
 from picamera2 import Picamera2
+from gpiozero import Servo
+from gpiozero import LED
+from gpiozero.pins.pigpio import PiGPIOFactory
+from time import sleep
+import math
+
+led_1 = LED(5)
+led_2 = LED(6)
+
+class servo_controller():
+    def __init__(self):
+        self.factory = PiGPIOFactory()
+        self.servo_1 = Servo(18, min_pulse_width = 0.5/1000, max_pulse_width = 2.5/1000, pin_factory = self.factory)
+        self.servo_2 = Servo(12, min_pulse_width = 0.5/1000, max_pulse_width = 2.5/1000, pin_factory = self.factory)
+    
+    def servo_1_setup(self):
+        dart = int(input("Press 1 for good dart and 2 for bad dart\n"))
+
+        if(dart == 1):
+            for i in range(0,40):
+                self.servo_1.value = math.sin(math.radians(i))
+                sleep(0.01)
+            sleep(1)
+            for i in range(40,-1,-1):
+                self.servo_1.value = math.sin(math.radians(i))
+                sleep(0.01)
+        elif(dart == 2):
+            for i in range(180,230):
+                self.servo_1.value = math.sin(math.radians(i))
+                sleep(0.01)
+            sleep(1)
+            for i in range(230,180,-1):
+                self.servo_1.value = math.sin(math.radians(i))
+                sleep(0.01)
+
+    def servo_2_setup(self):
+        self.servo_2.value = math.sin(math.radians(230))
+        sleep(3)
+        for i in range(230,200,-1):
+                self.servo_2.value = math.sin(math.radians(i))
+                sleep(0.01)
+        sleep(2)
+        for i in range(200,230):
+                self.servo_2.value = math.sin(math.radians(i))
+                sleep(0.01)
 
 def find_next_counter(path):
     files = os.listdir(path)
@@ -26,8 +71,16 @@ print("Press 'c' to capture an image, 'q' to quit")
 file_path = os.path.join(os.getcwd(), 'Pictures')
 counter = find_next_counter(file_path)
 
+controller = servo_controller()
+
 while True:
+    led_1.on()
+    led_2.on()
+    controller.servo_2_setup()
+    controller.servo_1_setup()
+    
     im = picam2.capture_array()
+
 
     cv2.imshow("Camera", im)
     c = cv2.waitKey(1)
