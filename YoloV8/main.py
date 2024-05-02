f11443201d8452af9f8ddd898fd9bc7b374bb3a3
Thomas Brinck-Mortensen <thomasbr@uia.no>
2024-05-02 21:30:14 +0200

    trainig+picture

--- YoloV8/main.py
+++ YoloV8/main.py
@@ -1,10 +1,18 @@
 from ultralytics import YOLO
 import os
+import torch
 
-os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"  # might be bad practice - used to circumwent: https://github.com/ultralytics/yolov5/issues/5086
+def main():
+    print(torch.cuda.is_available())
 
-# Load a model
-model = YOLO("yolov8n.yaml")  # build a new model from scratch
+    os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"  # might be bad practice - used to circumwent: https://github.com/ultralytics/yolov5/issues/5086
 
-# Use the model
-model.train(data=r"C:\Users\tbmor\BachelorData\SmartDart\YoloV8\dartV1.yaml", epochs=10)  # train the model, was preset to 3 - testing with 1 to save time
+    # Load a model
+    model = YOLO("yolov8s.yaml")  # build a new model from scratch
+    model.to('cuda')
+
+    # Use the model
+    model.train(data=r"C:\Users\tbmor\BachelorData\SmartDart\YoloV8\dartV1.yaml", epochs=250)  # train the model, was preset to 3 - testing with 1 to save time
+
+if __name__ == '__main__':
+    main()
